98133b08e83bf495fcc9a21863fa6fa6495bc4ac
Riseh4rd <112888563+Riseh4rd@users.noreply.github.com>
2022-09-13 17:29:57 +0300

    Create Veterinar.java

---
+++ Veterinar.java
@@ -0,0 +1,7 @@
+package HW_2;
+
+public class Veterinar {
+    public static void treatWIVOTNOE(WIVOTNOE WIVOTNOE) {
+        System.out.printf("%s\t%s\n",WIVOTNOE.EDA,WIVOTNOE.MESTOPOLOWENIE);
+    }
+}
