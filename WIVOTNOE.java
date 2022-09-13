50d55996d5921f6f251c3d6bdea832ba57d5f065
Riseh4rd <112888563+Riseh4rd@users.noreply.github.com>
2022-09-13 16:54:44 +0300

    Update WIVOTNOE.java

--- WIVOTNOE.java
+++ WIVOTNOE.java
@@ -1,23 +1,27 @@
-package HomeWork;
+package HW_2;
 
-public class Animal {
-    String food;
-    String location;
+public class WIVOTNOE {
+    
+String EDA;
+    
+String MESTOPOLOWENIE;
 
-    public Animal(String food, String location) {
-        this.food = food;
-        this.location = location;
-    }
-
-    public void makeNoise() {
-        System.out.println("Making some noise...");
-    }
-
-    public void eat() {
-        System.out.println("Eating...");
-    }
-
-    public void sleep() {
-        System.out.println("Sleeping...");
-    }
+public Animal(String EDA, String MESTOPOLOWENIE) {
+        
+this.MESTOPOLOWENIE = MESTOPOLOWENIE;
+        
+this.EDA = EDA;      
+}
+public void WUMET() {
+    
+System.out.println("Делает звуки и шумит"); 
+}
+public void KUWAT() {
+    
+System.out.println("Кушает");  
+}
+public void SPAT() {
+    
+System.out.println("Спит"); 
+} 
 }
