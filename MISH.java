eaccd024633af93fc2e6a729e1762b2baa6249b9
Riseh4rd <112888563+Riseh4rd@users.noreply.github.com>
2022-09-13 17:08:53 +0300

    Create MISH.java

---
+++ MISH.java
@@ -0,0 +1,19 @@
+package HW_2;
+
+public class MISH extends WIVOTNOE {
+    String NAZVANIE;
+    public MISH(String NAZVANIE, String EDA, String MESTOPOLOWENIE) {
+        super(EDA, MESTOPOLOWENIE);
+        this.NAZVANIE = NAZVANIE;
+    }
+
+    @Override
+    public void WUMET() {
+        System.out.println("Мышка пищит");
+    }
+
+    @Override
+    public void KUWAT() {
+        System.out.println("Мышка кушает семачку");
+    }
+}
