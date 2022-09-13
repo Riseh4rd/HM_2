3d05f5472785f98e040f2590fce50a4cf133faa1
Riseh4rd <112888563+Riseh4rd@users.noreply.github.com>
2022-09-13 17:01:11 +0300

    Create KOT.java

---
+++ KOT.java
@@ -0,0 +1,21 @@
+package HW_2;
+
+public class KOT extends WIVOTNOE {
+    String NAZVANIE;
+
+    public KOT(String NAZVANIE, String EDA, String MESTOPOLOWENIE) {
+        super(EDA, MESTOPOLOWENIE);
+        this.NAZVANIE = NAZVANIE;
+    }
+
+
+    @Override
+    public void WUMET() {
+        System.out.println("Кот издает звуки и шумит, прямо как настоящий кот!");
+    }
+
+    @Override
+    public void KUWAT() {
+        System.out.println("Кот кушает еду для котов!");
+    }
+}
